cefd323c1b5955a8d63c655a6285a708d5dd08f0
Darwin Bautista <daruuin@gmail.com>
2018-05-02 12:27:58 +0800

    Move find_homography() implementation to a separate file

--- dlt_test.py
+++ dlt_test.py
@@ -6,14 +6,12 @@ from keras.layers import Input
 from keras.models import Model
 import keras.backend as K
 
-import tensorflow as tf
-
-from homographynet.layers import DirectLinearTransform
+from homographynet.layers import Homography
 
 def main():
     p1 = Input((8, 1), name='p1')
     p2 = Input((8, 1), name='p2')
-    y = DirectLinearTransform()([p1, p2])
+    y = Homography()([p1, p2])
     m = Model([p1, p2], y)
     m.summary()
 
@@ -24,24 +22,18 @@ def main():
 
     data_p1 = np.array([[left,top,left+128,top,left+128,top+128,left,top+128]]).reshape(1,8,1)
     data_p2 = np.array([[-10, 4, 32, -8, -20, 5, 0, -15]]).reshape(1,8,1)
-    #labels = np.array([[[1,2,3],[4,5,6], [7,8,9]]])
     labels = np.array([[[ 7.02728475e-01,  3.96032036e-01, -4.81192937e+00],
        [-1.78547446e-01,  1.33162906e+00,  2.43080358e+00],
        [-2.13648383e-03,  3.22019432e-03,  1.00000000e+00]]])
 
     m.fit([data_p1, data_p2], labels, batch_size=1)
 
-    a = DirectLinearTransform().call([p1, p2])
+    a = Homography().call([p1, p2])
 
     sess = K.get_session()
 
     r = sess.run(a, feed_dict={p1: data_p1.astype('float32'), p2: data_p2.astype('float32')})
     print(r)
-
-
-
-
-
 
 
 if __name__ == '__main__':
