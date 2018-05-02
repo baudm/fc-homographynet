3510257b1d67b9f9042ebf03fe362e69f5600f4e
Darwin Bautista <daruuin@gmail.com>
2018-05-02 12:35:39 +0800

    Add Spatial Transformer code from Tensorflow

--- homographynet/layers.py
+++ homographynet/layers.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import tensorflow as tf
 from keras.layers import Layer
 
+from .vendor.spatial_transformer import transformer
 from .vendor.nguyen2017unsupervised import find_homography
 
 
@@ -23,3 +25,31 @@ class Homography(Layer):
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], 3, 3)
+
+
+class ImageTransformer(Layer):
+
+    def call(self, inputs, **kwargs):
+        I = inputs
+
+        # Transform H_mat since we scale image indices in transformer
+        H_mat = tf.matmul(tf.matmul(self.M_tile_inv, self.H_mat), self.M_tile)
+        # Transform image 1 (large image) to image 2
+        out_size = (self.params.img_h, self.params.img_w)
+        warped_images, _ = transformer(self.I, H_mat, out_size)
+        # TODO: warp image 2 to image 1
+
+        # Extract the warped patch from warped_images by flatting the whole batch before using indices
+        # Note that input I  is  3 channels so we reduce to gray
+        warped_gray_images = tf.reduce_mean(warped_images, 3)
+        warped_images_flat = tf.reshape(warped_gray_images, [-1])
+        patch_indices_flat = tf.reshape(self.patch_indices, [-1])
+        pixel_indices = patch_indices_flat + self.batch_indices_tensor
+        pred_I2_flat = tf.gather(warped_images_flat, pixel_indices)
+
+        self.pred_I2 = tf.reshape(pred_I2_flat,
+                                  [self.params.batch_size, self.params.patch_size, self.params.patch_size, 1])
+        return self.pred_I2
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], 128, 128)
