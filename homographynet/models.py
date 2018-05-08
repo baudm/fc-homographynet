b768b8d1bf31a80bdc14a5a245d40958078cb9c4
Darwin Bautista <darwin.bautista@eee.upd.edu.ph>
2018-05-08 12:00:48 +0800

    Add pretrained weights

--- homographynet/models.py
+++ homographynet/models.py
@@ -9,7 +9,7 @@ from keras.layers import Conv2D, Concatenate, Input, Reshape, Lambda
 
 from .layers import Homography, ImageTransformer
 
-MOBILENET_WEIGHTS_PATH = 'https://github.com/baudm/HomographyNet/raw/master/models/mobile_homographynet_weights_tf_dim_ordering_tf_kernels.h5'
+WEIGHTS_PATH = 'https://github.com/baudm/unsupervised-homographynet/raw/master/weights/hnet_weights_tf_dim_ordering_tf_kernels.h5'
 
 
 def create_models(use_weights=False):
@@ -25,6 +25,7 @@ def create_models(use_weights=False):
     offsets = Reshape((8, 1))(x)
     offsets_scaled = Lambda(lambda x: x * 32)(offsets)
 
+    # Additional inputs for unsupervised training
     full_image = Input((240, 320, 1), name='full_image')
     corners = Input((8, 1), name='corners')
 
@@ -32,14 +33,14 @@ def create_models(use_weights=False):
 
     warped = ImageTransformer(320, 240, patch_size)([full_image, H, corners])
 
-    train_model = Model([base_model.input, corners, full_image], warped, name='mobile_homographynet')
-    test_model = Model(base_model.input, offsets, name='mobile_homographynet_test')
+    train_model = Model([base_model.input, corners, full_image], warped, name='homographynet')
+    test_model = Model(base_model.input, offsets, name='homographynet_test')
 
     if use_weights:
-        weights_name = os.path.basename(MOBILENET_WEIGHTS_PATH)
-        weights_path = get_file(weights_name, MOBILENET_WEIGHTS_PATH,
-                                cache_subdir='models',
-                                file_hash='e161aabc5a04ff715a6f5706855a339d598d1216a4a5f45b90b8dbf5f8bcedc3')
+        weights_name = os.path.basename(WEIGHTS_PATH)
+        weights_path = get_file(weights_name, WEIGHTS_PATH,
+                                cache_subdir='weights',
+                                file_hash='4d52b0810d4c940dd8176a1ec1fb9641782c98570280a0ede38badc6e62288d0')
         test_model.load_weights(weights_path)
 
     return train_model, test_model
